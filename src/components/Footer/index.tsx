640d534b1ec862d8833b7b950185a1ba80c30703
devxmone <dmanh120419@gmail.com>
2024-10-21 15:58:23 +0700

    update social links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import telegramIcon from '../../assets/svg/telegram.svg'
 import logoWhite from '../../assets/svg/logo_white.svg'
 import mediumIcon from '../../assets/svg/medium.svg'
 import xIcon from '../../assets/svg/x.svg'
-import discordIcon from '../../assets/svg/discord.svg'
 
 const Footer = () => {
   return (
@@ -12,14 +12,24 @@ const Footer = () => {
         <p className="text-[12px] font-medium text-white">© 2024 BrownFi. All rights reserved.</p>
       </div>
       <div className="flex items-center">
-        <a className="ml-[40px] cursor-pointer">
+        <a
+          href="https://twitter.com/BrownFidex"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-[40px] cursor-pointer"
+        >
           <img src={xIcon} alt="ico" className="w-[40px]" />
         </a>
-        <a className="ml-[40px] cursor-pointer">
+        <a href="https://mirror.xyz/" target="_blank" rel="noopener noreferrer" className="ml-[40px] cursor-pointer">
           <img src={mediumIcon} alt="ico" className="w-[40px]" />
         </a>
-        <a className="ml-[40px] cursor-pointer">
-          <img src={discordIcon} alt="ico" className="w-[40px]" />
+        <a
+          href="https://t.me/brownfiamm"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-[40px] cursor-pointer"
+        >
+          <img src={telegramIcon} alt="ico" className="w-[40px]" />
         </a>
       </div>
     </div>
