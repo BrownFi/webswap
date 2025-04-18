4b4c45753f21442b574f66ef52a346756759279d
Khac Kien <ii.khackien@gmail.com>
2025-04-18 11:41:47 +0700

    Check display reverse pair

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -1,9 +1,9 @@
-import { ChainId, Currency, JSBI, Pair, Percent, TokenAmount } from '@brownfi/sdk'
+import { JSBI, Pair, Percent, TokenAmount } from '@brownfi/sdk'
 import { darken } from 'polished'
 import React, { useEffect, useState } from 'react'
 import { ChevronDown, ChevronUp } from 'react-feather'
 import { Link } from 'react-router-dom'
-import { Text } from 'rebass'
+import { Flex, Text } from 'rebass'
 import styled from 'styled-components'
 import { useTotalSupply } from '../../data/TotalSupply'
 
@@ -13,21 +13,20 @@ import { TYPE } from '../../theme'
 import { currencyId } from '../../utils/currencyId'
 import { unwrappedToken } from '../../utils/wrappedCurrency'
 import { ButtonPrimary, ButtonEmpty, ButtonSecondary } from '../Button'
-// import { CardNoise } from '../earn/styled'
 
 import { useColor } from '../../hooks/useColor'
 
 import Card, { GreyCard, LightCard } from '../Card'
 import { AutoColumn } from '../Column'
 import CurrencyLogo from '../CurrencyLogo'
-import DoubleCurrencyLogo from '../DoubleLogo'
+import DoubleCurrencyLogo, { DoubleCurrencySymbol } from '../DoubleLogo'
 import { RowBetween, RowFixed, AutoRow } from '../Row'
-import { Dots } from '../swap/styleds'
 import { BIG_INT_ZERO } from '../../constants'
 import { getTokenSymbol } from 'utils'
 import { useQuery } from '@tanstack/react-query'
 import { dexscreenerService, internalService } from 'services'
 import { formatPrice } from 'utils/prices'
+import { shouldReversePair } from 'utils/pair'
 
 export const FixedHeightRow = styled(RowBetween)`
   min-height: 24px;
@@ -107,8 +106,7 @@ export function MinimalPositionCard({ pair, showUnwrapped = false, border }: Pos
               <RowFixed>
                 <DoubleCurrencyLogo currency0={currency0} currency1={currency1} margin={true} size={20} />
                 <Text fontWeight={500} fontSize={20} color={'white'}>
-                  {getTokenSymbol(currency0, chainId)}/
-                  {chainId === ChainId.BOBA_MAINNET ? 'BOBA' : getTokenSymbol(currency1, chainId)}
+                  {getTokenSymbol(currency0, chainId)}/{getTokenSymbol(currency1, chainId)}
                 </Text>
               </RowFixed>
               <RowFixed>
@@ -142,7 +140,7 @@ export function MinimalPositionCard({ pair, showUnwrapped = false, border }: Pos
               </FixedHeightRow>
               <FixedHeightRow>
                 <Text fontSize={16} fontWeight={500} color={'white'}>
-                  {chainId === ChainId.BOBA_MAINNET ? 'BOBA' : getTokenSymbol(currency1, chainId)}:
+                  {getTokenSymbol(currency1, chainId)}:
                 </Text>
                 {token1Deposited ? (
                   <RowFixed>
@@ -178,6 +176,7 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
 
   const currency0 = unwrappedToken(pair.token0)
   const currency1 = unwrappedToken(pair.token1)
+  const shouldReverse = shouldReversePair(pair)
 
   const { data: token0Price = 0 } = useQuery({
     queryKey: ['getTokenPrice', pair.token0.address],
@@ -231,41 +230,17 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
       : [undefined, undefined]
 
   const backgroundColor = useColor(pair.token0)
-  const BOBA: Currency = {
-    decimals: 18,
-    symbol: 'BOBA',
-    name: 'Boba Token'
-  }
 
   return (
     <StyledPositionCard border={border} bgColor={backgroundColor}>
-      {/* <CardNoise /> */}
       <AutoColumn gap="12px">
         <FixedHeightRow>
           <AutoRow className="!w-fit" gap="8px">
             <div className="flex items-center gap-2 flex-wrap">
               <div className="flex items-center gap-2">
-                <DoubleCurrencyLogo
-                  currency0={currency0}
-                  currency1={chainId === ChainId.BOBA_MAINNET ? BOBA : currency1}
-                  size={20}
-                />
+                <DoubleCurrencyLogo currency0={currency0} currency1={currency1} size={20} />
                 <Text fontWeight={600} fontSize={20} className="text-white">
-                  {chainId !== ChainId.BOBA_MAINNET ? (
-                    !currency0 || !currency1 ? (
-                      <Dots>Loading</Dots>
-                    ) : (
-                      `${getTokenSymbol(currency0, chainId)}/${getTokenSymbol(currency1, chainId)}`
-                    )
-                  ) : null}
-                  {chainId === ChainId.SONIC_TESTNET &&
-                  getTokenSymbol(currency0, chainId) === 'DIAM' &&
-                  getTokenSymbol(currency1, chainId) === 'S'
-                    ? ' (FTM/USD)'
-                    : getTokenSymbol(currency0, chainId) === 'S' && getTokenSymbol(currency1, chainId) === 'CORAL'
-                    ? ' (FTM/ETH)'
-                    : ''}
-                  {chainId === ChainId.BOBA_MAINNET && 'USD/BOBA'}
+                  <DoubleCurrencySymbol currency0={currency0} currency1={currency1} />
                 </Text>
               </div>
               <div className="flex flex-wrap items-center gap-4">
@@ -346,39 +321,31 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
                 </Text>
               </FixedHeightRow>
 
-              <FixedHeightRow>
-                <div className="flex items-center gap-2">
-                  <CurrencyLogo currency={pair.token0} />
+              <Flex flexDirection={shouldReverse ? 'column-reverse' : 'column'} className="gap-2">
+                <FixedHeightRow>
+                  <div className="flex items-center gap-2">
+                    <CurrencyLogo currency={pair.token0} />
+                    <Text fontSize={16} fontWeight={500} color={'white'}>
+                      {getTokenSymbol(currency0, chainId)}
+                    </Text>
+                  </div>
                   <Text fontSize={16} fontWeight={500} color={'white'}>
-                    {getTokenSymbol(currency0, chainId)}
+                    {pair.reserve0.toSignificant(4)} <span className="text-[#949494]">({formatPrice(pool0Price)})</span>
                   </Text>
-                </div>
-                <Text fontSize={16} fontWeight={500} color={'white'}>
-                  {pair.reserve0.toSignificant(4)} <span className="text-[#949494]">({formatPrice(pool0Price)})</span>
-                </Text>
-              </FixedHeightRow>
-              <FixedHeightRow>
-                <div className="flex items-center gap-2">
-                  <CurrencyLogo currency={chainId === ChainId.BOBA_MAINNET ? BOBA : pair.token1} />
-                  <Text fontSize={16} fontWeight={500} color={'white'}>
-                    {chainId === ChainId.BOBA_MAINNET ? 'BOBA' : getTokenSymbol(currency1, chainId)}
-                  </Text>
-                </div>
-                <Text fontSize={16} fontWeight={500} color={'white'}>
-                  {pair.reserve1.toSignificant(4)} <span className="text-[#949494]">({formatPrice(pool1Price)})</span>
-                </Text>
-              </FixedHeightRow>
+                </FixedHeightRow>
 
-              {chainId === ChainId.SONIC_TESTNET && pair.token0.symbol === 'DIAM' && pair.token1.symbol === 'WS' && (
-                <Text fontWeight={500} fontSize={14} color={'#ffffff'} marginTop={'8px'}>
-                  Pair S/Diamond = FTM/USD
-                </Text>
-              )}
-              {chainId === ChainId.SONIC_TESTNET && pair.token1.symbol === 'CORAL' && pair.token0.symbol === 'WS' && (
-                <Text fontWeight={500} fontSize={14} color={'#ffffff'} marginTop={'8px'}>
-                  Pair S/CORAL = FTM/ETH
-                </Text>
-              )}
+                <FixedHeightRow>
+                  <div className="flex items-center gap-2">
+                    <CurrencyLogo currency={pair.token1} />
+                    <Text fontSize={16} fontWeight={500} color={'white'}>
+                      {getTokenSymbol(currency1, chainId)}
+                    </Text>
+                  </div>
+                  <Text fontSize={16} fontWeight={500} color={'white'}>
+                    {pair.reserve1.toSignificant(4)} <span className="text-[#949494]">({formatPrice(pool1Price)})</span>
+                  </Text>
+                </FixedHeightRow>
+              </Flex>
             </>
 
             {account && (
@@ -404,47 +371,49 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
                   )}
                 </FixedHeightRow>
 
-                <FixedHeightRow>
-                  <RowFixed className="gap-2">
-                    <CurrencyLogo currency={currency0} />
-                    <Text fontSize={16} fontWeight={500} color={'white'}>
-                      Pooled {getTokenSymbol(currency0, chainId)}
-                    </Text>
-                  </RowFixed>
-                  {token0Deposited ? (
+                <Flex flexDirection={shouldReverse ? 'column-reverse' : 'column'} className="gap-2">
+                  <FixedHeightRow>
                     <RowFixed className="gap-2">
+                      <CurrencyLogo currency={currency0} />
                       <Text fontSize={16} fontWeight={500} color={'white'}>
-                        {token0Deposited?.toSignificant(4)}
-                      </Text>
-                      <Text fontSize={16} fontWeight={500} color={'#949494'}>
-                        ({formatPrice(token0Price * Number(token0Deposited.toSignificant(4)))})
+                        Pooled {getTokenSymbol(currency0, chainId)}
                       </Text>
                     </RowFixed>
-                  ) : (
-                    '-'
-                  )}
-                </FixedHeightRow>
+                    {token0Deposited ? (
+                      <RowFixed className="gap-2">
+                        <Text fontSize={16} fontWeight={500} color={'white'}>
+                          {token0Deposited?.toSignificant(4)}
+                        </Text>
+                        <Text fontSize={16} fontWeight={500} color={'#949494'}>
+                          ({formatPrice(token0Price * Number(token0Deposited.toSignificant(4)))})
+                        </Text>
+                      </RowFixed>
+                    ) : (
+                      '-'
+                    )}
+                  </FixedHeightRow>
 
-                <FixedHeightRow>
-                  <RowFixed className="gap-2">
-                    <CurrencyLogo currency={chainId === ChainId.BOBA_MAINNET ? BOBA : currency1} />
-                    <Text fontSize={16} fontWeight={500} color={'white'}>
-                      Pooled {chainId === ChainId.BOBA_MAINNET ? 'BOBA' : getTokenSymbol(currency1, chainId)}
-                    </Text>
-                  </RowFixed>
-                  {token1Deposited ? (
+                  <FixedHeightRow>
                     <RowFixed className="gap-2">
+                      <CurrencyLogo currency={currency1} />
                       <Text fontSize={16} fontWeight={500} color={'white'}>
-                        {token1Deposited?.toSignificant(4)}
-                      </Text>
-                      <Text fontSize={16} fontWeight={500} color={'#949494'}>
-                        ({formatPrice(token1Price * Number(token1Deposited.toSignificant(4)))})
+                        Pooled {getTokenSymbol(currency1, chainId)}
                       </Text>
                     </RowFixed>
-                  ) : (
-                    '-'
-                  )}
-                </FixedHeightRow>
+                    {token1Deposited ? (
+                      <RowFixed className="gap-2">
+                        <Text fontSize={16} fontWeight={500} color={'white'}>
+                          {token1Deposited?.toSignificant(4)}
+                        </Text>
+                        <Text fontSize={16} fontWeight={500} color={'#949494'}>
+                          ({formatPrice(token1Price * Number(token1Deposited.toSignificant(4)))})
+                        </Text>
+                      </RowFixed>
+                    ) : (
+                      '-'
+                    )}
+                  </FixedHeightRow>
+                </Flex>
 
                 <FixedHeightRow>
                   <Text fontSize={16} fontWeight={500} color={'white'}>
@@ -463,11 +432,7 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
               <ButtonPrimary
                 padding="8px"
                 as={Link}
-                to={`/add/${currencyId(currency0)}/${
-                  chainId === ChainId.BOBA_MAINNET
-                    ? '0xa18bF3994C0Cc6E3b63ac420308E5383f53120D7'
-                    : currencyId(currency1)
-                }`}
+                to={`/add/${currencyId(currency0)}/${currencyId(currency1)}`}
                 width="48%"
               >
                 Add
@@ -477,11 +442,7 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
                   padding="8px"
                   as={Link}
                   width="48%"
-                  to={`/remove/${currencyId(currency0)}/${
-                    chainId === ChainId.BOBA_MAINNET
-                      ? '0xa18bF3994C0Cc6E3b63ac420308E5383f53120D7'
-                      : currencyId(currency1)
-                  }`}
+                  to={`/remove/${currencyId(currency0)}/${currencyId(currency1)}`}
                 >
                   Remove
                 </ButtonPrimary>
