4b7e991c976d8c42082fa82bf9feba8ae411f61d
Khac Kien <ii.khackien@gmail.com>
2025-07-01 13:22:11 +0700

    Try add env

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -50,8 +50,10 @@ arbitrum.iconUrl = require('assets/images/arb.png')
 // @ts-ignore
 sepolia.iconUrl = require('assets/images/ethereum-logo.png')
 
+console.log('======== ENV:', process.env.REACT_APP_ENV)
 // const availableChains =
 export const availableChains: Chain[] = [arbitrumSepolia, arbitrum, sepolia]
+// export const availableChains: Chain[] = [berachain, arbitrum, viction, u2uMainnet]
 export const getDefaultChain = (index?: number): Chain => availableChains[index ?? 0]
 
 export const injected = new InjectedConnector({
