43ec28072c113580d56bcfe9fd2e2fec4148bfcf
Khac Kien <ii.khackien@gmail.com>
2025-07-01 12:49:57 +0700

    Remove unused env

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,37 +1,13 @@
-import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from './WalletConnector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
-import { arbitrumSepolia, berachain, boba, sepolia, viction } from 'wagmi/chains'
+import { arbitrum, arbitrumSepolia, berachain, mainnet, sepolia, viction } from 'wagmi/chains'
 
-import { NetworkConnector } from './NetworkConnector'
 import UNISWAP_LOGO_URL from '../assets/svg/logo.svg'
 import { ChainId } from '@brownfi/sdk'
 import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { defineChain } from 'viem'
-
-const ARBITRUM_SEPOLIA_URL = process.env.REACT_APP_ARBITRUM_SEPOLIA_URL
-
-/** @deprecated */
-export const NETWORK_CHAIN_ID: number = ChainId.ARBITRUM_SEPOLIA
-
-/** @deprecated */
-export const NETWORK_URL = ARBITRUM_SEPOLIA_URL
-
-if (typeof NETWORK_URL === 'undefined') {
-  throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
-}
-
-/** @deprecated */
-export const network = new NetworkConnector({
-  urls: { [NETWORK_CHAIN_ID]: NETWORK_URL }
-})
-
-let networkLibrary: Web3Provider | undefined
-export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
-}
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
@@ -41,13 +17,13 @@ export const walletconnect = new WalletConnectConnector({
 
 // mainnet only
 export const walletlink = new WalletLinkConnector({
-  url: NETWORK_URL,
+  url: mainnet.rpcUrls.default.http[0],
   appName: 'Brownfi',
   appLogoUrl: UNISWAP_LOGO_URL
 })
 
 // @ts-ignore
-const u2uMainnet: Chain = defineChain({
+export const u2uMainnet: Chain = defineChain({
   id: 39,
   name: 'U2U Network',
   nativeCurrency: { decimals: 18, name: 'U2U', symbol: 'U2U' },
@@ -58,23 +34,25 @@ const u2uMainnet: Chain = defineChain({
   },
   blockExplorers: {
     default: { name: 'U2U Scan', url: 'https://u2uscan.xyz' }
-  }
+  },
+  iconUrl: require('assets/images/u2u.jpg')
 })
 
 // @ts-ignore
 berachain.iconUrl = require('assets/images/w-bera.png')
 // @ts-ignore
-boba.iconUrl = require('assets/images/boba.svg').default
-// @ts-ignore
 viction.iconUrl = require('assets/images/viction.png')
-// @ts-ignore
-u2uMainnet.iconUrl = require('assets/images/u2u.jpg')
+
 // @ts-ignore
 arbitrumSepolia.iconUrl = require('assets/images/arb.png')
 // @ts-ignore
+arbitrum.iconUrl = require('assets/images/arb.png')
+// @ts-ignore
 sepolia.iconUrl = require('assets/images/ethereum-logo.png')
 
-export const availableChains: Chain[] = [arbitrumSepolia, sepolia]
+// const availableChains =
+export const availableChains: Chain[] = [arbitrumSepolia, arbitrum, sepolia]
+export const getDefaultChain = (index?: number): Chain => availableChains[index ?? 0]
 
 export const injected = new InjectedConnector({
   supportedChainIds: availableChains.map(chain => chain.id)
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,24 @@
 import { ChainId, ChainIdHex, JSBI, Percent, Token, WETH } from '@brownfi/sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
-import { injected, walletconnect, walletlink } from '../connectors'
+import { injected, u2uMainnet, walletconnect, walletlink } from '../connectors'
+import {
+  arbitrum,
+  arbitrumSepolia,
+  auroraTestnet,
+  baseSepolia,
+  bscTestnet,
+  metis,
+  soneiumMinato,
+  sonicTestnet,
+  taikoHekla,
+  unichainSepolia,
+  viction,
+  victionTestnet,
+  optimism,
+  boba,
+  berachain
+} from 'viem/chains'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
@@ -222,7 +239,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'Viction',
       symbol: 'VIC'
     },
-    rpcUrls: [process.env.REACT_APP_VICTION_MAINNET_URL]
+    rpcUrls: viction.rpcUrls.default.http
   },
   [ChainId.BSC_TESTNET]: {
     chainId: ChainIdHex[ChainId.BSC_TESTNET],
@@ -233,7 +250,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'BNB',
       symbol: 'BNB'
     },
-    rpcUrls: [process.env.REACT_APP_BSC_TESTNET_URL]
+    rpcUrls: bscTestnet.rpcUrls.default.http
   },
   [ChainId.VICTION_TESTNET]: {
     chainId: ChainIdHex[ChainId.VICTION_TESTNET],
@@ -244,7 +261,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'Viction',
       symbol: 'VIC'
     },
-    rpcUrls: [process.env.REACT_APP_VICTION_TESTNET_URL]
+    rpcUrls: victionTestnet.rpcUrls.default.http
   },
   [ChainId.SONIC_TESTNET]: {
     chainId: ChainIdHex[ChainId.SONIC_TESTNET],
@@ -255,7 +272,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'Sonic',
       symbol: 'S'
     },
-    rpcUrls: [process.env.REACT_APP_SONIC_TESTNET_URL]
+    rpcUrls: sonicTestnet.rpcUrls.default.http
   },
   [ChainId.MINATO_SONEIUM]: {
     chainId: ChainIdHex[ChainId.MINATO_SONEIUM],
@@ -266,7 +283,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_MINATO_SONEIUM_URL]
+    rpcUrls: soneiumMinato.rpcUrls.default.http
   },
   [ChainId.BASE_SEPOLIA]: {
     chainId: ChainIdHex[ChainId.BASE_SEPOLIA],
@@ -277,7 +294,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'USDC',
       symbol: 'USDC'
     },
-    rpcUrls: [process.env.REACT_APP_BASE_SEPOLIA_URL]
+    rpcUrls: baseSepolia.rpcUrls.default.http
   },
   [ChainId.UNICHAIN_SEPOLIA]: {
     chainId: ChainIdHex[ChainId.UNICHAIN_SEPOLIA],
@@ -288,7 +305,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_UNICHAIN_SEPOLIA_URL]
+    rpcUrls: unichainSepolia.rpcUrls.default.http
   },
   [ChainId.AURORA_TESTNET]: {
     chainId: ChainIdHex[ChainId.AURORA_TESTNET],
@@ -299,7 +316,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_AURORA_TESTNET_URL]
+    rpcUrls: auroraTestnet.rpcUrls.default.http
   },
   [ChainId.METIS_MAINNET]: {
     chainId: ChainIdHex[ChainId.METIS_MAINNET],
@@ -310,7 +327,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'Metis',
       symbol: 'METIS'
     },
-    rpcUrls: [process.env.REACT_APP_METIS_MAINNET_URL]
+    rpcUrls: metis.rpcUrls.default.http
   },
   [ChainId.TAIKO_TESTNET]: {
     chainId: ChainIdHex[ChainId.TAIKO_TESTNET],
@@ -321,7 +338,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_TAIKO_TESTNET_URL]
+    rpcUrls: taikoHekla.rpcUrls.default.http
   },
   [ChainId.U2U_MAINNET]: {
     chainId: ChainIdHex[ChainId.U2U_MAINNET],
@@ -332,7 +349,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'U2U',
       symbol: 'U2U'
     },
-    rpcUrls: [process.env.REACT_APP_U2U_MAINNET_URL]
+    rpcUrls: u2uMainnet.rpcUrls.default.http
   },
   [ChainId.ARBITRUM_SEPOLIA]: {
     chainId: ChainIdHex[ChainId.ARBITRUM_SEPOLIA],
@@ -343,7 +360,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_ARBITRUM_SEPOLIA_URL]
+    rpcUrls: arbitrumSepolia.rpcUrls.default.http
   },
   [ChainId.ARBITRUM_MAINNET]: {
     chainId: ChainIdHex[ChainId.ARBITRUM_MAINNET],
@@ -354,7 +371,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_ARBITRUM_MAINNET_URL]
+    rpcUrls: arbitrum.rpcUrls.default.http
   },
   [ChainId.OP_MAINNET]: {
     chainId: ChainIdHex[ChainId.OP_MAINNET],
@@ -365,7 +382,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_OP_MAINNET_URL]
+    rpcUrls: optimism.rpcUrls.default.http
   },
   [ChainId.BOBA_MAINNET]: {
     chainId: ChainIdHex[ChainId.BOBA_MAINNET],
@@ -376,7 +393,7 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'ETH',
       symbol: 'ETH'
     },
-    rpcUrls: [process.env.REACT_APP_BOBA_MAINNET_URL]
+    rpcUrls: boba.rpcUrls.default.http
   },
   [ChainId.BERA_MAINNET]: {
     chainId: ChainIdHex[ChainId.BERA_MAINNET],
@@ -387,6 +404,6 @@ export const CHAIN_TO_METAMASK: any = {
       name: 'Wrapped Bera',
       symbol: 'WBERA'
     },
-    rpcUrls: [process.env.REACT_APP_BERA_MAINNET_URL]
+    rpcUrls: berachain.rpcUrls.default.http
   }
 }
