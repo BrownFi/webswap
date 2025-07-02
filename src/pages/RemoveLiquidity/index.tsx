9f094884f9fc8c5b34d5c5afaa784226f3ffd579
Khac Kien <ii.khackien@gmail.com>
2025-07-02 16:28:40 +0700

    Fix remove lp

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -131,7 +131,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' }
     ]
     const domain = {
-      name: 'Uniswap V2',
+      name: 'BrownFi V2',
       version: '1',
       chainId: chainId,
       verifyingContract: pair.liquidityToken.address
