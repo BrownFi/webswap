7d0ff105c1e17e4ae03bc2440e8642d2b5ee5f98
Khac Kien <ii.khackien@gmail.com>
2025-07-10 09:54:15 +0700

    Turn off reverse

--- src/utils/pair.ts
+++ src/utils/pair.ts
@@ -1,10 +1,12 @@
 import { Pair } from '@brownfi/sdk'
 
 export const shouldReverse = (pair: string) => {
-  return ['USDC.e/BERA', 'BERA/iBGT', 'BERA/LBGT'].includes(pair)
+  return false
+  // return ['USDC.e/BERA', 'BERA/iBGT', 'BERA/LBGT'].includes(pair)
 }
 
 export const shouldReversePair = (pair: Pair) => {
-  const pairSymbols = [pair.token0.symbol, pair.token1.symbol].join('/')
-  return ['USDC.e/WBERA', 'WBERA/iBGT', 'WBERA/LBGT'].includes(pairSymbols)
+  return false
+  // const pairSymbols = [pair.token0.symbol, pair.token1.symbol].join('/')
+  // return ['USDC.e/WBERA', 'WBERA/iBGT', 'WBERA/LBGT'].includes(pairSymbols)
 }
