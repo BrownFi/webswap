ba58524d731e382c0ca44aabf315b55ee336fb49
Khac Kien <ii.khackien@gmail.com>
2025-07-11 16:38:27 +0700

    Update pair trading fee v2

--- src/hooks/useTradingFee.tsx
+++ src/hooks/useTradingFee.tsx
@@ -1,16 +1,21 @@
 import { Pair } from '@brownfi/sdk'
 import { useEffect, useState } from 'react'
+import { useVersion } from './useVersion'
+import { useActiveWeb3React } from 'hooks'
 
 type Props = {
   pair: Pair
 }
 
 export const useTradingFee = ({ pair }: Props) => {
+  const { chainId } = useActiveWeb3React()
+  const { version } = useVersion({ chainId })
+
   const [tradingFee, setTradingFee] = useState(0)
 
   useEffect(() => {
     const getTradingFee = async () => {
-      setTradingFee((await pair.getTradingFee()) * 2)
+      setTradingFee((await pair.getTradingFee()) * (version === 1 ? 2 : 1))
     }
     getTradingFee()
   }, [pair.liquidityToken.address])
