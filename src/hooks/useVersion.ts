360e0142b15c3df01e68860226e3799e996ea9f8
Khac Kien <ii.khackien@gmail.com>
2025-09-23 16:17:57 +0700

    Enable BSC

--- src/hooks/useVersion.ts
+++ src/hooks/useVersion.ts
@@ -63,7 +63,7 @@ export function useVersion({ chainId }: { chainId: number | undefined | null }) 
         ChainId.BERA_MAINNET,
         ChainId.ARBITRUM_MAINNET,
         ChainId.BASE_MAINNET,
-        // ChainId.BSC_MAINNET,
+        ChainId.BSC_MAINNET,
         ChainId.HYPER_EVM,
         ChainId.LINEA_MAINNET,
       ].includes(chainId as number) && version === 2
